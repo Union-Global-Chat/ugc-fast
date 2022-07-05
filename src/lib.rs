3a90e1c857599e46deea8092c84b4db5b0da3538
mc_fdc <Masato.04.11.2007@gmail.com>
2022-07-05 14:05:35 +0900

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,37 @@
-
 use pyo3::prelude::*;
+use native_tls::{TlsConnector, TlsStream};
+use tungstenite::protocol::WebSocket;
+
+use std::thread;
+use std::net::TcpStream;
 
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
     Ok((a + b).to_string())
+}
+
+#[pyclass]
+struct FastProtocol {
+    ws: Option<WebSocket<TlsStream<TcpStream>>>,
+    pub closed: bool,
+    loop_: PyObject,
+}
+
+#[pymethods]
+impl FastProtocol {
+    #[new]
+    fn new(loop_: PyObject) Self {
+        FastProtocol {
+            ws: None,
+            closed: true,
+            loop_: PyObject,
+        }
+    }
+    
+    fn connect(&mut self) {
+        let future = self.loop_.call_method0("create_future");
+    }
 }
 
 /// A Python module implemented in Rust.
